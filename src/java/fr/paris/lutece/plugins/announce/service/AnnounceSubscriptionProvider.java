7196ff31770d8f88f6cbc4687550da467a5464d3
jtidaf <juba.tidaf@sogeti.com>
2019-04-04 19:04:40 +0200

    [SITEANNONC-29] solve the subscription problem when a user subscribes to himself

--- src/java/fr/paris/lutece/plugins/announce/service/AnnounceSubscriptionProvider.java
+++ src/java/fr/paris/lutece/plugins/announce/service/AnnounceSubscriptionProvider.java
@@ -455,7 +455,7 @@ public class AnnounceSubscriptionProvider implements ISubscriptionProviderServic
      */
     private boolean hasSubscribedtoResource( LuteceUser user, String strIdResource, String strSubscriptionKey )
     {
-        SubscriptionFilter filter = new SubscriptionFilter( user.getUserInfo( LuteceUser.BUSINESS_INFO_ONLINE_EMAIL ), getProviderName( ), strSubscriptionKey,
+        SubscriptionFilter filter = new SubscriptionFilter( user.getName(), getProviderName( ), strSubscriptionKey,
                 strIdResource );
         List<Subscription> listSubscription = SubscriptionService.getInstance( ).findByFilter( filter );
 
