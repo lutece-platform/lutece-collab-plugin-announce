a56c51c4151cd2643d9a1fe880f73520433a85d4
ClaraFlo2 <clara.florac@paris.fr>
2026-02-23 18:01:33 +0100

    LUT-32008 : (V7) Refactoring - Add foreign key constraints

--- src/sql/plugins/announce/plugin/create_db_announce.sql
+++ src/sql/plugins/announce/plugin/create_db_announce.sql
@@ -104,3 +104,15 @@ id_announce int default '0' NOT NULL,
 PRIMARY KEY (id)
 );
 
+ALTER TABLE announce_category
+  ADD CONSTRAINT fk_category_sector
+  FOREIGN KEY (id_sector) REFERENCES announce_sector(id_sector) ON DELETE RESTRICT;
+
+ALTER TABLE announce_announce
+  ADD CONSTRAINT fk_announce_category
+  FOREIGN KEY (id_category) REFERENCES announce_category(id_category) ON DELETE RESTRICT;
+
+ALTER TABLE announce_notify
+  ADD CONSTRAINT fk_notify_announce
+  FOREIGN KEY (id_announce) REFERENCES announce_announce(id_announce) ON DELETE CASCADE;
+
